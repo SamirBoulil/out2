fc8559e44f57197a27119816547246aec38d2d7e
Samir Boulil <samir.boulil@akeneo.com>
2017-11-30 18:36:17 +0100

    Add user repository

---
+++ src/OnceUponATime/Domain/Repository/UserRepositoryInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnceUponATime\Domain\Repository;
+
+use OnceUponATime\Domain\Entity\User;
+use OnceUponATime\Domain\Entity\UserId;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface UserRepositoryInterface
+{
+    public function add(User $user): void;
+
+    public function byId(UserId $userId): User;
+
+    public function all(): array;
+
+    public function nextIdentity(): UserId;
+}
