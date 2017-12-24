5bb9fef8809df08cb9a3c8ef1a2bcb9b8fedaa70
Samir Boulil <samir.boulil@akeneo.com>
2017-12-24 19:32:59 +0100

    Add Application container

---
+++ src/OnceUponATime/Infrastructure/Common/OnceUponATimeApplicationContainer.php
@@ -0,0 +1,159 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OnceUponATime\Infrastructure\Common;
+
+use OnceUponATime\Application\AnswerQuestion\AnswerQuestionHandler;
+use OnceUponATime\Application\AnswerQuestion\QuestionAnsweredNotify;
+use OnceUponATime\Application\AskQuestion\AskQuestionHandler;
+use OnceUponATime\Application\AskQuestion\QuestionAskedNotify;
+use OnceUponATime\Application\AskQuestion\QuizCompletedNotify;
+use OnceUponATime\Application\RegisterUser\RegisterUserHandler;
+use OnceUponATime\Application\RegisterUser\UserRegisteredNotify;
+use OnceUponATime\Domain\Event\QuizEventStore;
+use OnceUponATime\Domain\Repository\QuestionRepository;
+use OnceUponATime\Domain\Repository\UserRepository;
+use OnceUponATime\Infrastructure\Notifications\NewQuestionForNewUser;
+use OnceUponATime\Infrastructure\Notifications\NewQuestionForUserWhoAnsweredCorrectly;
+use OnceUponATime\Infrastructure\Notifications\NewQuestionForUserWhoAnsweredIncorrectly;
+use OnceUponATime\Infrastructure\Notifications\PublishToEventStore;
+use OnceUponATime\Infrastructure\Notifications\QuestionAnsweredNotifyMany;
+use OnceUponATime\Infrastructure\Notifications\QuestionAskedNotifyMany;
+use OnceUponATime\Infrastructure\Notifications\QuizCompletedNotifyMany;
+use OnceUponATime\Infrastructure\Notifications\UserRegisteredNotifyMany;
+use OnceUponATime\Infrastructure\Persistence\InMemory\InMemoryQuestionRepository;
+use OnceUponATime\Infrastructure\Persistence\InMemory\InMemoryQuizEventStore;
+use OnceUponATime\Infrastructure\Persistence\InMemory\InMemoryUserRepository;
+use OnceUponATime\Infrastructure\UI\CLI\RegisterUserConsoleHandler;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class OnceUponATimeApplicationContainer implements ContainerInterface
+{
+    /** @var array */
+    private $container = [];
+
+    public function __construct()
+    {
+        $this->container = $this->buildContainer();
+    }
+
+    public function get($id)
+    {
+        return $this->container->get($id);
+    }
+
+    public function has($id): bool
+    {
+        return $this->container->has($id);
+    }
+
+    public function getConsoleApplication(): Application
+    {
+        return $this->container->get(Application::class);
+    }
+
+    private function buildContainer(): ContainerInterface
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        /**
+         * Persistence
+         */
+        $containerBuilder->register(UserRepository::class, InMemoryUserRepository::class);
+        $containerBuilder->register(QuestionRepository::class, InMemoryQuestionRepository::class);
+        $containerBuilder->register(QuizEventStore::class, InMemoryQuizEventStore::class);
+
+        /**
+         * Notify
+         */
+        $containerBuilder
+            ->register(UserRegisteredNotify::class, UserRegisteredNotifyMany::class)
+            ->addArgument([new Reference(NewQuestionForNewUser::class)]);
+        $containerBuilder->register(NewQuestionForNewUser::class, NewQuestionForNewUser::class)
+            ->addArgument(new Reference(AskQuestionHandler::class));
+
+        $containerBuilder
+            ->register(QuestionAnsweredNotify::class, QuestionAnsweredNotifyMany::class)
+            ->addArgument( new Reference(PublishToEventStore::class))
+            ->addArgument( new Reference(NewQuestionForUserWhoAnsweredCorrectly::class))
+            ->addArgument( new Reference(NewQuestionForUserWhoAnsweredIncorrectly::class));
+        $containerBuilder->register(PublishToEventStore::class, PublishToEventStore::class)
+            ->addArgument(new Reference(QuizEventStore::class));
+        $containerBuilder->register(NewQuestionForUserWhoAnsweredCorrectly::class, NewQuestionForUserWhoAnsweredCorrectly::class)
+            ->addArgument( new Reference(AskQuestionHandler::class));
+        $containerBuilder->register(NewQuestionForUserWhoAnsweredIncorrectly::class,
+            NewQuestionForUserWhoAnsweredIncorrectly::class)
+            ->addArgument( new Reference(QuizEventStore::class))
+            ->addArgument( new Reference(AskQuestionHandler::class));
+
+        $containerBuilder
+            ->register(QuestionAskedNotify::class, QuestionAskedNotifyMany::class)
+            ->addArgument([new Reference(PublishToEventStore::class)]);
+
+        $containerBuilder
+            ->register(QuizCompletedNotify::class, QuizCompletedNotifyMany::class)
+            ->addArgument([new Reference(PublishToEventStore::class)]);
+
+        /**
+         * Handler
+         */
+        $containerBuilder
+            ->register(AnswerQuestionHandler::class, AnswerQuestionHandler::class)
+            ->addArgument(new Reference(UserRepository::class))
+            ->addArgument(new Reference(QuestionRepository::class))
+            ->addArgument(new Reference(QuizEventStore::class))
+            ->addArgument(new Reference(QuestionAnsweredNotify::class));
+        $containerBuilder
+            ->register(AskQuestionHandler::class, AskQuestionHandler::class)
+            ->addArgument(new Reference(UserRepository::class))
+            ->addArgument(new Reference(QuestionRepository::class))
+            ->addArgument(new Reference(QuizEventStore::class))
+            ->addArgument(new Reference(QuizCompletedNotify::class))
+            ->addArgument(new Reference(QuestionAskedNotify::class));
+        $containerBuilder
+            ->register(RegisterUserHandler::class, RegisterUserHandler::class)
+            ->addArgument( new Reference(UserRepository::class))
+            ->addArgument( new Reference(UserRegisteredNotify::class));
+        $containerBuilder
+            ->register(ShowClueHandler::class, ShowClueHandler::class)
+            ->addArgument( new Reference(UserRepository::class))
+            ->addArgument( new Reference(QuestionRepository::class))
+            ->addArgument( new Reference(QuizEventStore::class));
+        $containerBuilder
+            ->register(ShowLeaderboardHandler::class, ShowLeaderboardHandler::class)
+            ->addArgument( new Reference(UserRepository::class))
+            ->addArgument( new Reference(QuizEventStore::class));
+        $containerBuilder
+            ->register(ShowQuestionHandler::class, ShowQuestionHandler::class)
+            ->addArgument( new Reference(UserRepository::class))
+            ->addArgument( new Reference(QuestionRepository::class))
+            ->addArgument( new Reference(QuizEventStore::class));
+
+        /**
+         * Command
+         */
+        $containerBuilder
+            ->register(RegisterUserConsoleHandler::class, RegisterUserConsoleHandler::class)
+            ->addArgument(new Reference(RegisterUserHandler::class));
+
+        /**
+         * Application
+         */
+        $containerBuilder
+            ->register(Application::class, Application::class)
+            ->addMethodCall('add', [new Reference(RegisterUserConsoleHandler::class)])
+            ->setPublic(true);
+
+        $containerBuilder->compile();
+
+        return $containerBuilder;
+    }
+}
